e24ce2fc054858e2d99e30ca25487513784ef81f
Artiom Zelianok <artemzelenok60@gmail.com>
2025-11-30 00:59:54 +0300

    Add in database Role

---
+++ WebProject/Enum/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace WebProject.Enum
+{
+    public enum Role
+    {
+        User = 1,
+        SuperUser = 2,
+        Admin = 3,
+        SuperAdmin = 4,
+    }
+}
---
+++ WebProject/Migrations/20251129215537_AddRole.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using WebProject.Enum;
+
+#nullable disable
+
+namespace WebProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: Role.User);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+        }
+    }
+}
