d9f2ec5166e9f551269a9dc34c900d673023cff9
ArtemZEL <artemzelenok60@gmail.com>
2026-04-05 15:52:50 +0300

    Add Statistics and mini update localization page

---
+++ WebProject/DBStuff/RAWSql/CoffeShopSqlHelper.sql
@@ -0,0 +1,17 @@
+--search on add coffee on with author
+
+SELECT U.UserName AS AuthorName,
+    CF.Name AS CoffeeName,
+    CF.Cell AS Price
+FROM CoffeeProducts CF
+    LEFT JOIN Users U ON U.Id = CF.AuthorId
+ORDER BY U.UserName, CF.Name;
+
+--search
+SELECT 
+    U.UserName AS AuthorName,
+    COUNT(*) AS TotalCoffees
+FROM CoffeeProducts CF
+    LEFT JOIN Users U ON U.Id = CF.AuthorId
+GROUP BY U.UserName
+ORDER BY U.UserName;
--- WebProject/DBStuff/Repositories/Interface/ICoffeeRepository.cs
+++ WebProject/DBStuff/Repositories/Interface/ICoffeeRepository.cs
@@ -1,9 +1,12 @@
 ﻿using WebProject.DBStuff.Models.CoffeShop;
+using WebProject.Models.CoffeeShop;
 
 namespace WebProject.DBStuff.Repositories.Interface
 {
     public interface ICoffeeRepository:IBaseRepository<CoffeeProductDB>
     {
         IEnumerable<CoffeeProductDB> GetAllWithAuthors();
+        List<CoffeDetailsViewModel> GetCoffeeDetail();
+        List<CoffeeSummaryViewModel> GetCoffeeSummary();
     }
 }
