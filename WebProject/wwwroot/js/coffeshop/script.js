38c7b64271b82cf8aac10f050d426549c989ce3f
ArtemZEL <artemzelenok60@gmail.com>
2026-04-07 10:29:19 +0300

    Add Miniscripts

--- WebProject/wwwroot/js/coffeshop/script.js
+++ WebProject/wwwroot/js/coffeshop/script.js
@@ -23,3 +23,27 @@ let headers = document.querySelector('header');
 window.addEventListener('scroll',()=>{
     headers.classList.toggle('shadow',window.scrollY > 0);
 })
+
+// $(document).ready(function()
+// {
+//     let $search = $(".search-box");
+//     let $navbar = $(".navbar");
+//     let $header = $("header");
+
+//     $("#search-icon").on("click",function(){
+//         $search.toggleClass("active");
+//         $navbar.removeClass("active");
+//     });
+
+//     $("#menu-icon").on("click",function(){
+//         $navbar.toggleClass("active");
+//         $search.removeClass("active");
+//     });
+
+//     $(window).on("scroll",function(){
+//         $search.removeClass("active");
+//         $navbar.removeClass("active");
+//         $header.toggleClass("shadow",$(window).scrollTop>0);
+//     });
+// })
+
